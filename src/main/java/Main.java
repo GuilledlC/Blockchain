b401a878f3405d407f8896024fd89eb4894c8a63
alephandro <alex29561@gmail.com>
2024-02-21 00:16:20 +0100

    Add data generation method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
 import database.Database;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
             //File file = new File("./src/main/resources/publicKeys.txt");
+            //generateData(file);
             Database database = new Database();
+            //database.loadData(file);
             System.out.println(database.getValue("27384915E"));
             database.closeDatabase();
         } catch (IOException error){
             error.printStackTrace();
         }
+    }
+
+    private void generateData(File dnis) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(dnis));
+        for(int i = 10000000; i < 40000000; ++i) {
+            String dni = i + "E\n";
+            bw.write(dni);
+        }
+        bw.close();
     }
 }
