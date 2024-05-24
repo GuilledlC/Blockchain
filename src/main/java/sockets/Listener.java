7c8aa169679d0addc50b5cb051872e73a42f4c14
GuilledlC <guillermodelacalhacar@gmail.com>
2024-05-24 19:20:00 +0200

    Add new Architecture (WIP)

--- src/main/java/sockets/Listener.java
+++ src/main/java/sockets/Listener.java
@@ -9,17 +9,22 @@ import java.util.HashMap;
 
 public class Listener {
 
+	//Nodos que deberiamos tener hardcodeados
     private static ArrayList<Socket> bootstrapNodes;
+
+	//Nodos que se conectan a nosotros
     private ArrayList<PeerHandler> connectedNodes;
+
+	//El socket a traves del cual escuchamos
     private final ServerSocket serverSocket;
 
     public Listener(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
         bootstrapNodes = new ArrayList<>();
         connectedNodes = new ArrayList<>();
-        //bootstrapNodes.add(new Socket("localhost", 8008));
     }
 
+	//Un nuevo Thread a traves del cual recibimos conexiones y las inicializamos a sus propios Threads
     public void startListening() {
         new Thread(() -> {
             try {
@@ -35,12 +40,14 @@ public class Listener {
 
     }
 
+	//Metodo para conectarse a una IP
     public void connectTo(String ip, int port) throws IOException {
         Socket peerSocket = new Socket(ip, port);
         System.out.println("Connected to peer at " + peerSocket.getInetAddress() + ":" + peerSocket.getPort());
         handlePeer(peerSocket);
     }
 
+	//Metodo para crear un Thread con la conexion
     private void handlePeer(Socket peerSocket) {
         PeerHandler peer = new PeerHandler(peerSocket);
         Thread peerThread = new Thread(peer);
@@ -48,14 +55,17 @@ public class Listener {
         connectedNodes.add(peer);
     }
 
+	//Metodo para enviar objetos a todos nuestros Peers
     public void sendObject(Serializable object) {
         PeerHandler.sendObjects(object);
     }
 
+	//Metodo para recoger todos los objetos que nos han mandado nuestros Peers
     public ArrayList<Object> getObjects() {
         return PeerHandler.getObjects();
     }
 
+	//Metodo para cerrar la conexion
     private void closeListener() {
         try {
             serverSocket.close();
