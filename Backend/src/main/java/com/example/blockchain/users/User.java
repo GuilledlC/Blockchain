d0afe99f3dc198fc63522ecbfcef1c5d94805681
GuilledlC <guillermodelacalhacar@gmail.com>
2024-06-12 12:32:23 +0200

    Aqui funciona

--- Backend/src/main/java/com/example/blockchain/users/User.java
+++ Backend/src/main/java/com/example/blockchain/users/User.java
@@ -31,7 +31,7 @@ public class User implements Serializable {
 	private void initializeBootstrapNodes() {
 		bootstrapNodes.add("88.27.144.170");
 		bootstrapNodes.add("80.39.151.138");
-		//bootstrapNodes.add("2.153.80.40");
+		bootstrapNodes.add("2.153.80.40");
 	}
 
 	public void vote(String receiver) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
