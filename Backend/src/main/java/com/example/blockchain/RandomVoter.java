9b4392e36038d985b1c4ddc80a09c05e0d24b340
GuilledlC <guillermodelacalhacar@gmail.com>
2024-06-25 20:35:20 +0200

    parallelize sending votes

--- Backend/src/main/java/com/example/blockchain/RandomVoter.java
+++ Backend/src/main/java/com/example/blockchain/RandomVoter.java
@@ -9,17 +9,39 @@ import java.nio.file.Paths;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 public class RandomVoter {
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, InvalidKeyException, InterruptedException {
-		for(int i = 0; i < 100000; i++) {
+		/*for(int i = 0; i < 100000; i++) {
 			PublicKey pubk = KeyUtils.publicKeyReader(
 					Files.readAllBytes(Paths.get("keys/" + i + ".pub")));
 			PrivateKey privk = KeyUtils.privateKeyReader(
 					Files.readAllBytes(Paths.get("keys/" + i + ".key")));
 			User user = new User(privk, pubk);
 			user.vote("" + i);
-			Thread.sleep((new Random()).nextInt(0, 5) * 10L);
-		}
+		}*/
+
+		ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+		IntStream.range(0, 100000)
+				.forEach(i -> CompletableFuture.supplyAsync(() -> {
+					try {
+						PublicKey pubk = KeyUtils.publicKeyReader(
+								Files.readAllBytes(Paths.get("keys/" + i + ".pub")));
+						PrivateKey privk = KeyUtils.privateKeyReader(
+								Files.readAllBytes(Paths.get("keys/" + i + ".key")));
+						User user = new User(privk, pubk);
+						user.vote("" + i);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					return null;
+				}, executor));
+
+		executor.shutdown();
 	}
 }
